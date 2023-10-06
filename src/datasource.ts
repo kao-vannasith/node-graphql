5cc047f48aa25c4dda59bd8c090a956a3e9cbdb7
kao-vannasith <vannnasith@gmail.com>
2023-10-06 14:46:29 +0700

    first commit

---
+++ src/datasource.ts
@@ -0,0 +1,18 @@
+/* eslint-disable prettier/prettier */
+export default [
+  {
+    email: 'tlannister@got.com',
+    firstName: 'Tywin',
+    id: '1',
+    lastName: 'Lannister',
+    password: 'my-very-secured-password',
+  },
+  {
+    email: 'lmormont@got.com',
+    firstName: 'Lyanna',
+    id: '2',
+    lastName: 'Mormont',
+    password: 'my-very-secured-password',
+  },
+
+];
---
+++ src/index.ts
@@ -0,0 +1,24 @@
+/* eslint-disable prettier/prettier */
+import { readFileSync } from 'fs';
+import { resolve } from 'path';
+import { startStandaloneServer } from '@apollo/server/standalone';
+import { ApolloServer } from '@apollo/server';
+import { resolvers } from './resolvers';
+
+// eslint-disable-next-line @typescript-eslint/ban-types
+type ApolloContext = {};
+
+const GRAPHQL_SCHEMA_PATH = resolve(__dirname, 'schema.graphql');
+
+const typeDefs = readFileSync(GRAPHQL_SCHEMA_PATH, { encoding: 'utf-8' });
+
+const server = new ApolloServer<ApolloContext>({
+  typeDefs,
+  resolvers,
+});
+
+startStandaloneServer(server, {
+  listen: { port: 4000 },
+}).then((result) => {
+  console.log(`🚀 Server ready at: ${result.url}`);
+});
